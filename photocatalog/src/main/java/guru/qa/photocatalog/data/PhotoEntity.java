3440f05ab9b06136bf137b0913cb160b758d0dd1
filkot <filkot@gmail.com>
2025-03-20 23:35:57 +0300

    Initial commit

---
+++ photocatalog/src/main/java/guru/qa/photocatalog/data/PhotoEntity.java
@@ -0,0 +1,58 @@
+package guru.qa.photocatalog.data;
+
+import jakarta.persistence.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "photos")
+public class PhotoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, columnDefinition = "UUID default gen_random_uuid()")
+    private UUID id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "last_modify_date", columnDefinition = "DATE", nullable = false)
+    private Date lastModifyDate;
+
+    @Column(name = "content", columnDefinition = "bytea")
+    private byte[] content;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getLastModifyDate() {
+        return lastModifyDate;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        PhotoEntity that = (PhotoEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
